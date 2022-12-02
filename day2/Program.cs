4268f14f873e7d7dfcd7c536a586633723ad7548
František Jukl <jukl@proconom.cz>
2022-12-02 21:10:15 +0100

    day2 part2

--- day2/Program.cs
+++ day2/Program.cs
@@ -10,55 +10,29 @@ abstract class Choice
     public abstract Options Option { get; }
     public abstract int Value { get; }
 
-    public abstract int Score(Choice choice);
+    public abstract Dictionary<Options, int> Score { get; }
 }
 
 class RockChoice : Choice
 {
     public override Options Option => Options.Rock;
     public override int Value => 1;
-    public override int Score(Choice choice)
-    {
-        return choice.Option switch
-        {
-            Options.Rock => 3,
-            Options.Paper => 0,
-            Options.Scissors => 6,
-            _ => throw new ArgumentException("nope")
-        };
-    }
+
+    public override Dictionary<Options, int> Score => new() { { Options.Rock, 3 }, { Options.Paper, 0 }, { Options.Scissors, 6 } };
 }
 
 class PaperChoice : Choice
 {
     public override Options Option => Options.Paper;
     public override int Value => 2;
-    public override int Score(Choice choice)
-    {
-        return choice.Option switch
-        {
-            Options.Rock => 6,
-            Options.Paper => 3,
-            Options.Scissors => 0,
-            _ => throw new ArgumentException("nope")
-        };
-    }
+    public override Dictionary<Options, int> Score => new() { { Options.Rock, 6 }, { Options.Paper, 3 }, { Options.Scissors, 0 } };
 }
 
 class ScissorsChoice : Choice
 {
     public override Options Option => Options.Scissors;
     public override int Value => 3;
-    public override int Score(Choice choice)
-    {
-        return choice.Option switch
-        {
-            Options.Rock => 0,
-            Options.Paper => 6,
-            Options.Scissors => 3,
-            _ => throw new ArgumentException("nope")
-        };
-    }
+    public override Dictionary<Options, int> Score => new() { { Options.Rock, 0 }, { Options.Paper, 6 }, { Options.Scissors, 3 } };
 }
 
 class Round
@@ -72,7 +46,7 @@ class Round
         this.OpponentChoice = opponentChoice;
     }
 
-    public int Score => MyChoice.Score(OpponentChoice) + MyChoice.Value;
+    public int Score => MyChoice.Score[OpponentChoice.Option] + MyChoice.Value;
 }
 
 class ChoiceFactory
@@ -88,13 +62,28 @@ class ChoiceFactory
         };
     }
 
-    // public Choice Make(char choiceChar, char outcomeChar)
-    // {
-    //     // return outcomeChar switch
-    //     // {
-    //     //     'X' => this.Make(choiceChar).Score
-    //     // }
-    // }
+    public Choice Make(char choiceChar, char outcomeChar)
+    {
+        Options option = outcomeChar switch
+        {
+            'X' => this.Make(choiceChar).Score.Single(x => x.Value == 6).Key,
+            'Y' => this.Make(choiceChar).Score.Single(x => x.Value == 3).Key,
+            'Z' => this.Make(choiceChar).Score.Single(x => x.Value == 0).Key,
+            _ => throw new ArgumentException("nope")
+        };
+        return this.MakeByOption(option);
+    }
+
+    private Choice MakeByOption(Options option)
+    {
+        return option switch
+        {
+            Options.Rock => new RockChoice(),
+            Options.Paper => new PaperChoice(),
+            Options.Scissors => new ScissorsChoice(),
+            _ => throw new ArgumentException("nope")
+        };
+    }
 }
 
 class Program
@@ -109,6 +98,7 @@ class Program
         Console.WriteLine(rounds.Sum(x => x.Score));
 
         // PART 2
-
+        rounds = lines.Select(x => new Round(factory.Make(x[0], x[2]), factory.Make(x[0]))).ToList();
+        Console.WriteLine(rounds.Sum(x => x.Score));
     }
 }
