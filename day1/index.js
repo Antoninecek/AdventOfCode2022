67cb0c3a84dcec05ffee88f90b087623634a0e2d
František Jukl <jukl@proconom.cz>
2022-12-02 15:20:23 +0100

    day1

---
+++ day1/index.js
@@ -0,0 +1,14 @@
+import fs from 'fs/promises';
+
+const r = async () => {
+    const data = await fs.readFile('./input', { encoding: 'utf8' });
+    return data.split('\n\n');
+}
+
+const data = await r();
+const max = Math.max(...data.map(x => {
+    const red = x.split('\n').filter(y => y !== '').reduce((a, b) => parseInt(a) + parseInt(b), 0);
+    return red;
+}));
+
+console.log(max);
