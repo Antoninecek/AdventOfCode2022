19b76d42623ba04e2be9fd6d595370151110a48a
František Jukl <jukl@proconom.cz>
2022-12-05 18:29:11 +0100

    full day3

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,6 +1,14 @@
 ﻿namespace day3;
 class Program
 {
+    static int GetPriority(char ch)
+    {
+        int val = (int)ch;
+        if (97 <= val && val <= 122) val -= 96;
+        if (65 <= val && val <= 90) val -= 38;
+        return val;
+    }
+
     static void Main(string[] args)
     {
         string[] lines = File.ReadAllLines("../day3input");
@@ -15,15 +23,24 @@ class Program
             {
                 if (b.Contains(a[j]))
                 {
-                    int val = (int)a[j];
-                    if (97 <= val && val <= 122) val -= 96;
-                    if (65 <= val && val <= 90) val -= 38;
-                    sum += val;
+                    sum += GetPriority(a[j]);
                     continue;
                 }
             }
         }
 
+        Console.WriteLine(sum);
+
+        sum = 0;
+        for (int i = 0; i < lines.Length; i += 3)
+        {
+            string a = lines[i];
+            string b = lines[i + 1];
+            string c = lines[i + 2];
+
+            IEnumerable<char> inter = a.Intersect(b.Intersect(c));
+            sum += GetPriority(inter.Single());
+        }
         Console.WriteLine(sum);
     }
 }
